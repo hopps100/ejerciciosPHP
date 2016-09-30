f906dcccdfe21a8cff1fe6a261c3da14afd7838a
ivan hernandez <hernandezgz@hotmail.es>
2016-09-30 18:28:18 +0200

    primer ej

---
+++ ejercicio01.php
@@ -0,0 +1,27 @@
+ <!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo 6</title>
+</head><body>
+<?php
+/*Crea un script(ejercicio01.php) 
+Debes crear un array ordenado de  elementos. 
+Inicializarlo al declararlo.
+Muestra su contenido en un bucke for*/
+echo " Ejercicio01 <hr>";
+$array1 = array (
+    'id' => 200,
+    'nombre' => 'Iván',
+    'apellido' => 'Hernández Ocejo',
+    'edad' => 19 ,
+    'nacionalidad' => 'Española'
+);
+
+
+//foreach clave->elemento
+echo "<br>";
+foreach ($array1 as $position=>$element){
+    echo $position . ": " . $element . '<br>';
+}
+?>
+</body></html>
